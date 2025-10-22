80f018bcd4062de3b9f70a64595cc1dbc893fe4e
$GITLAB_USER_LOGIN <$GITLAB_USER_EMAIL>
2025-10-22 12:22:24 +0100

    ok

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -19,7 +19,7 @@
       docker images -q | xargs -r docker rmi -f
       docker system prune -f  
       docker compose ps -q | grep . >/dev/null && docker compose down || echo "no runing docker comopose container"
-      docker compose up --build . 
+      docker compose up --build 
     '';
   };
 }
