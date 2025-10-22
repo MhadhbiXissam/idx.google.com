4efcb7a0772fdee21e2458ca87abb27506ef5950
$GITLAB_USER_LOGIN <$GITLAB_USER_EMAIL>
2025-10-22 11:53:07 +0100

    ok

---
+++ .idx/dev.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }: {
+  services.docker.enable = true;
+
+  channel = "stable-23.11";
+
+  packages = with pkgs; [
+    python3
+    gnumake
+  ];
+
+  env = {
+    GIT_USER = "mhadhbixissam";
+
+  };
+
+  idx.workspace.onStart = {
+    docker-setup = ''
+      docker images -q | xargs -r docker rmi -f
+      docker system prune -f  
+      docker compose up 
+
+
+    '';
+  };
+}
